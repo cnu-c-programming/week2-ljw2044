aa9fb0aa6eb02c9321fb0ad94bd7ae1cf56d3acc
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-16 12:27:51 +0900

    4-1c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+    
+    if (op == '+') {
+        printf("%d\n", a + b);
+    }
+    else if (op == '-') {
+        printf("%d\n", a - b);
+    }
+    else if (op == '*') {
+        printf("%d\n", a * b);
+    }
+    else if (op == '/') {
+        printf("%d\n", a / b);
+    }
 
     return 0;
 }
-
