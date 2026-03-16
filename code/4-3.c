5e6b4a3c8b1658f0a85ca191d8fcd8c037234c36
ljw2044 <144752394+ljw2044@users.noreply.github.com>
2026-03-16 12:48:48 +0900

    4-3c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,30 @@
 
 int main()
 {
+    int n;
+    int i;
+    int Prime = 1;
+
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        Prime = 0;
+    }
+    else {
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                Prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (Prime == 1) {
+        printf("true\n");
+    }
+    else {
+        printf("false\n");
+    }
 
     return 0;
 }
-
